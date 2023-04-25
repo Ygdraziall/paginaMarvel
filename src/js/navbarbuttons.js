866758227fc62cce9d09d355089c1fb726c2db78
Ygdraziall <101606277+Ygdraziall@users.noreply.github.com>
2023-04-25 00:16:29 -0300

    actualizacion de los listados

--- src/js/navbarbuttons.js
+++ src/js/navbarbuttons.js
@@ -1,16 +1,57 @@
 const navButton = document.getElementById('navButton');
 const navExtrasHome = document.getElementById('navExtrasHome');
-let navMenuOpen = false;
 
-/*navButton.addEventListener('mouseenter', () => {
-    console.log('sapo')}
-    )
 
-navButton.addEventListener('mouseleave', () => {
-        console.log('sapo')}
-        )*/
+
+let navMenuisOpen = false;
+let navMenuOnOver = false;
+
+
+
+const showMenu = () => {
+    if(!navMenuisOpen) {
+        navMenuisOpen.hidden = false
+        navExtrasHome.hiden = true
+    }
+}
+
+const closeMenu = () => {
+    setTimeout(() => {
+        if(navMenuisOpen && !navMenuOnOver){
+            navExtrasHome.hidden = true
+            navMenuisOpen = false
+        }
+    }, 1000);
+}
+
+const StayMenu = () =>{
+
+    if(navMenuisOpen){
+        navExtrasHome.hidden = false
+        navMenuOnOver = true
+    }
+}
+
+const leaveMenu = () => {
+    navMenuOnOver = false
+    setTimeout(() => {
+        if(navMenuisOpen){
+            navExtrasHome.hidden = true
+            navMenuisOpen = false}
+    }, 1000);
+}
+
+
 navButton.addEventListener('mouseenter', () => {
-    navExtrasHome.hidden = false}    )
+    navExtrasHome.hidden = false})
         
 navButton.addEventListener('mouseleave', () => {
-    navExtrasHome.hidden = true})   
+    setTimeout(() =>   { 
+    navExtrasHome.hidden = true}, 1000 );})  
+    
+navButton.addEventListener('mouseenter', showMenu)
+navButton.addEventListener('mouseleave', closeMenu)
+navExtrasHome.addEventListener('mouseenter', StayMenu)
+navExtrasHome.addEventListener('mouseleave', leaveMenu)
+
+/*primero agrego las funciones ya que JAVASCRIPT al igual que CSS lee todo de arroiba para abajo y despues si se ejcuta*/
